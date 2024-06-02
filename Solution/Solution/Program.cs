45302ede10d76bc23b93f7d745435f720883fc1c
nisslyak <nissopodolyak@gmail.com>
2024-06-02 05:52:51 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿class Result
+{
+    public static int ReverseInt(int num)
+    {
+        int result = 0;
+        while (num > 0)
+        {
+            result = result * 10 + num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+    public static bool IsPalindrome(int x)
+    {
+        int reversedInt = Result.ReverseInt(x);
+
+        if (x != reversedInt)
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
+    }
+}
+
+
+class Solution
+{
+    public static void Main(String[] args)
+    {
+        int input = Convert.ToInt32(Console.ReadLine());
+        bool isPalindrome = Result.IsPalindrome(input);
+    }
+}
